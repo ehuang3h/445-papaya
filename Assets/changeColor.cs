f4aee426fc395011f30a6871889f9a99e03ca4fd
Emily Huang <emily_huang_5@sfu.ca>
2024-02-08 14:37:59 -0800

    copied assets folder from unity VR tutorial

---
+++ Assets/changeColor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+
+    public Material blue;
+    public Material red;
+    private Camera mainCamera;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, 1000))
+        {
+            if (hit.collider && hit.collider.gameObject == cube) {
+                foreach(Renderer renderer in hit.collider.GetComponentsInChildren<Renderer>())
+                {
+                    renderer.material = red;
+                }
+            }
+        }
+    }
+}
