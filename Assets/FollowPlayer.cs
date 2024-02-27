21db7979100de66dea94fd83e6555d205fe3f997
jspr-p <jasper.precilla@gmail.com>
2024-02-27 00:19:36 -0800

    Added character following logic and a trigger to startFollow

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    private float dist;
+    public float followRadius;
+    
+    private Boolean follow = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (follow)
+        {
+            dist = Vector3.Distance(transform.position, player.transform.position);
+            if (dist > followRadius)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+                Debug.Log("Following.");
+            }
+            else Debug.Log("Max distance reached. Dist: " +dist);
+        }
+        else Debug.Log("Not following.");
+    }
+
+    public void startFollowing()
+    {
+        follow = true;
+    }
+
+    public void stopFollowing()
+    {
+        follow = false;
+    }
+}
